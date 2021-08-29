458b7609d10331c9c87762b85d2fc28a28ca2bab
Sivan Kaplan <sivanagar@gmail.com>
2021-08-29 16:01:24 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,7 @@ var saveTasks = function() {
 };
 
 var auditTask = function(taskEl) {
+  console.log(taskEl)
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
 
@@ -185,7 +186,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -224,16 +225,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(this).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(this).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
   update: function(event) {
     // array to store the task data in
@@ -288,5 +293,11 @@ $("#modalDueDate").datepicker({
   
 
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 
